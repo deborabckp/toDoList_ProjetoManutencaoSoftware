3ed2fb9fba607a777c41e45a87d2ca14dc177694
deborabckp <debora.costa22396@alunos.ufersa.edu.br>
2025-07-15 21:04:16 -0300

    chore: removendo pasta env do repositório e atualizando a .gitignore

--- core/serializers.py
+++ core/serializers.py
@@ -1,16 +1,14 @@
 """Serializers do app core para o model Task."""
-
 from rest_framework import serializers
 from core.models import Task
 
 # pylint: disable=too-few-public-methods
 class TaskSerializer(serializers.ModelSerializer):
-    """Serializer para o model Task.
-
+    """
     Princípio I (Segregação de Interface - ISP):
-    Este serializer só expõe os campos necessários (id, title, done, created_at),
-    evitando depender de métodos ou atributos que não são usados.
-    
+    Este serializer define uma interface enxuta para o modelo Task,
+    mostrando apenas os campos necessários para a API e ocultando outros 
+    atributos do modelo que não são tão relevantes para esta camada
     """
 
     class Meta:
